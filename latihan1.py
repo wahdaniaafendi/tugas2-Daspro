27cee8e7c1a3a7dbc63c6c05f791bc3c143dc314
wahdania.niA005@gmail.com <wahdania.nia005@gmail.com>
2024-03-06 17:20:20 +0900

    first commit

---
+++ latihan1.py
@@ -0,0 +1,10 @@
+nama = "wahdania"
+umur = "18"
+berat_badan = " 50"
+tinggi_badan = "158"
+
+print(f"nama saya: {nama}")
+print(f"umur saya: {umur} tahun")
+print(f"berat_badan saya: {berat_badan} kg")
+print(f"tinggi_badan saya: {tinggi_badan} cm")
+
